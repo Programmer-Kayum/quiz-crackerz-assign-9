2f70b1cceb5837c2fcd0e2b2bb6b6e7183b29e8c
unknown <mohammadkayumislam@gmail.com>
2022-10-12 07:57:06 +0600

    home page responsive slv

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,30 +10,30 @@ const Home = () => {
   return (
 
 <div>
-              <div className='px-4 py-8 mx-auto bg-gray-300 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10'>
-              <div className='flex flex-col items-center justify-between lg:flex-row'>
+              <div className='px-4 py-8 mx-auto bg-gray-300 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-32'>
+              <div className='flex flex-col  items-center justify-between lg:flex-row'>
 
               <div className='mb-10 lg:max-w-lg  lg:pr-5 lg:mb-0'>
               <div className='max-w-xl lg:mb-6  mt-5 '>
                 
-                    <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none'>
+                    <h2 className='max-w-lg mb-6 font-sans lg:text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none ml-20'>
                     Test <br className='hidden md:block' /><span className='text-orange-400'>Your</span> <span className='inline-block text-blue-400'>Talent</span>
                     </h2>
-                    <h3 className='md:text-lg lg:text-3xl font-bold'>
+                    <h3 className='md:text-lg lg:text-3xl ml-10 font-bold'>
                     The happiest feeling in the world is when you set a goal and accomplish it!.
                     </h3>
                </div>
                </div>
-                <div className='relative lg:w-1/2 '>
-                <div className='w-full lg:w-4/5 lg:mt-5 sm:mt-0 lg:ml-auto h-56'>
-                    <img className='h-60' src={img1} alt="" />
+                {/* <div className='lg:w-1/2 '> */}
+                <div className='w-full lg:w-2/4  sm:mt-0 lg:ml-auto h-56'>
+                    <img className='lg:h-60  lg:w-full' src={img1} alt="" />
                 </div>
-                </div>
+                {/* </div> */}
               </div>
                </div>
 
 
-              <div className=' lg:grid-cols-2  lg:w-[900px] sm:grid grid-cols-1  my-5 lg:mx-80 gap-4 lg:px-20  sm:max-w-xl sm:ml-5 md:max-w-full  md:px-24 '>
+              <div className=' lg:grid-cols-2 md:grid-cols-2 lg:w-[900px] sm:grid grid-cols-1  my-5 lg:mx-80 gap-4 lg:px-20  sm:max-w-xl sm:ml-5 md:max-w-full  md:px-24 '>
               {
               quizTopics.map(quiz=> <Quiz
                 key={quiz.id}
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,12 +7,12 @@ const Quiz = ({quiz}) => {
 const {logo, name,total,id}= quiz;
 
     return (
-        <div className='h-[355px] lg:w-[320px] border-4 sm:mx-auto rounded-lg static bg-gray-400'>
+        <div className='h-[365px] lg:w-[320px] border-4 sm:mx-auto rounded-lg static bg-gray-400'>
             <img className='h-[300px] p-5 mx-auto ' src={logo} alt="" />
-            <div className='flex justify-between bg-blue-400'>
+            <div className='flex mb-0 pb-0 justify-between bg-blue-400'>
                 <div className='flex justify-between'>
                 <h3 className=' mx-5 text-2xl font-bold'>{name}</h3>
-                <p className=' text-2xl font-bold'>{total}</p>
+                <p className=' text-2xl font-bold'> <small className='text-sm'>Quiz :{total}</small> </p>
                 
                 </div>
                 <div>
